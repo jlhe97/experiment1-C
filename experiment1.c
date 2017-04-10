813f7c03fc90ff096b80c72e2f71516f0250792b
jlhe97 <jlhe0197@gmail.com>
2017-04-10 15:52:45 -0400

    changes to the experiment, find the process in the heap

--- experiment1.c
+++ experiment1.c
@@ -1,31 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-typedef struct scrub{
+typedef struct student{
     char* name;
+    int len_name;
     char* lastname;
-    int age;
-    int graduationYear;
+    int len_lastname;
     char* major1;
+    int len_major1;
     char* major2;
+    int len_major2;
 
-} scrub;
+} student;
 
 int main(int argc, char* argv[]){
 
-    while(1){
+    student* foo_stud = (student*) malloc(sizeof(student));
 
-    scrub fooScrub;
+    char* line = NULL;
+    size_t size;
+    
+    printf("What is your first name? ");
 
-    fooScrub.name = "Juan Luis";
-    fooScrub.lastname = "Herrero Estrada";
-    fooScrub.age = 20;
-    fooScrub.graduationYear = 2019;
-    fooScrub.major1 = "Computer Science";
-    fooScrub.major2 = "Mathematics";
+    // first we get the first name
+    if (getline(&line, &size, stdin) == -1){ 
+        printf("No input\n");
+    } else { // correct
+        printf("first name: %s\n", line);
+        foo_stud->name = line;
+        foo_stud->len_name = strlen(line);
+        printf("What is your last name? ");
+        
+        
+        if (getline(&line, &size, stdin) == -1){     
+            printf("No input\n");
+        } else { // correct
+            printf("last name: %s\n", line);
+            foo_stud->lastname = line;
+            foo_stud->len_lastname = strlen(line);
+            printf("What is your first major? ");
 
-    }
+            
+            if (getline(&line, &size, stdin) == -1){     
+                printf("No input\n");
+            } else { // correct
+                printf("first major: %s\n", line);
+                foo_stud->major1 = line;
+                foo_stud->len_major1 = strlen(line);
+                printf("What is your second major? ");
+                
+
+                if (getline(&line, &size, stdin) == -1){     
+                    printf("No input\n");
+                } else { // correct
+                    printf("second major %s\n", line);
+                    foo_stud->major2 = line;
+                    foo_stud->len_major2 = strlen(line);
+                    printf("program succesfully finished\n");
+                }
+            }
+        }
+    } 
+
+
+
+
 
 
     return 0;
